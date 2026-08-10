64155ffc7f0372f5fe0a625f1cc41a8c3d953de7
aiwanaitsolution-cyber <aiwanaitsolution-cyber@users.noreply.github.com>
2026-08-10 20:51:46 +0530

    Use local partner logo assets

--- components/AssociationLogoMarquee.tsx
+++ components/AssociationLogoMarquee.tsx
@@ -1,22 +1,22 @@
 import Image from "next/image";
 
 export const associationLogos = [
-  { name: "Bank of Baroda", src: "https://logo.clearbit.com/bankofbaroda.bank.in", href: "https://bankofbaroda.bank.in/" },
-  { name: "Canara Bank", src: "https://logo.clearbit.com/canarabank.bank.in", href: "https://www.canarabank.bank.in/" },
-  { name: "State Bank of India", src: "https://logo.clearbit.com/sbi.co.in", href: "https://sbi.bank.in/" },
-  { name: "HDFC Bank", src: "https://logo.clearbit.com/hdfcbank.com", href: "https://www.hdfcbank.com/" },
-  { name: "ICICI Bank", src: "https://logo.clearbit.com/icicibank.com", href: "https://www.icicibank.com/" },
-  { name: "Axis Bank", src: "https://logo.clearbit.com/axisbank.com", href: "https://www.axisbank.com/" },
-  { name: "Kotak Mahindra Bank", src: "https://logo.clearbit.com/kotak.com", href: "https://www.kotak.com/" },
-  { name: "IDFC First Bank", src: "https://logo.clearbit.com/idfcfirstbank.com", href: "https://www.idfcfirstbank.com/" },
-  { name: "IndusInd Bank", src: "https://logo.clearbit.com/indusind.com", href: "https://www.indusind.com/" },
-  { name: "Deutsche Bank", src: "https://logo.clearbit.com/db.com", href: "https://www.db.com/" },
-  { name: "Saraswat Bank", src: "https://logo.clearbit.com/saraswatbank.com", href: "https://www.saraswatbank.com/" },
-  { name: "AU Small Finance Bank", src: "https://logo.clearbit.com/au.bank.in", href: "https://www.au.bank.in/" },
-  { name: "Bajaj Finserv", src: "https://logo.clearbit.com/bajajfinserv.in", href: "https://www.bajajfinserv.in/" },
-  { name: "Aditya Birla Finance", src: "https://logo.clearbit.com/adityabirlacapital.com", href: "https://finance.adityabirlacapital.com/" },
-  { name: "Ambit Finvest", src: "https://logo.clearbit.com/ambit.co", href: "https://finvest.ambit.co/about-us" },
-  { name: "Jio Credit", src: "https://logo.clearbit.com/jiocredit.in", href: "https://www.jiocredit.in/" }
+  { name: "Bank of Baroda", src: "/assets/partners/bank-of-baroda.png", href: "https://bankofbaroda.bank.in/" },
+  { name: "Canara Bank", src: "/assets/partners/canara-bank.png", href: "https://www.canarabank.bank.in/" },
+  { name: "State Bank of India", src: "/assets/partners/state-bank-of-india.png", href: "https://sbi.bank.in/" },
+  { name: "HDFC Bank", src: "/assets/partners/hdfc-bank.png", href: "https://www.hdfcbank.com/" },
+  { name: "ICICI Bank", src: "/assets/partners/icici-bank.png", href: "https://www.icicibank.com/" },
+  { name: "Axis Bank", src: "/assets/partners/axis-bank.jpg", href: "https://www.axisbank.com/" },
+  { name: "Kotak Mahindra Bank", src: "/assets/partners/kotak-mahindra-bank.jpg", href: "https://www.kotak.com/" },
+  { name: "IDFC First Bank", src: "/assets/partners/idfc-first-bank.jpg", href: "https://www.idfcfirstbank.com/" },
+  { name: "IndusInd Bank", src: "/assets/partners/indusind-bank.jpg", href: "https://www.indusind.com/" },
+  { name: "Deutsche Bank", src: "/assets/partners/deutsche-bank.png", href: "https://www.db.com/" },
+  { name: "Saraswat Bank", src: "/assets/partners/saraswat-bank.png", href: "https://www.saraswatbank.com/" },
+  { name: "AU Small Finance Bank", src: "/assets/partners/au-small-finance-bank.png", href: "https://www.au.bank.in/" },
+  { name: "Bajaj Finserv", src: "/assets/partners/bajaj-finserv.png", href: "https://www.bajajfinserv.in/" },
+  { name: "Aditya Birla Finance", src: "/assets/partners/aditya-birla-finance.jpg", href: "https://finance.adityabirlacapital.com/" },
+  { name: "Ambit Finvest", src: "/assets/partners/ambit-finvest.webp", href: "https://finvest.ambit.co/about-us" },
+  { name: "Jio Credit", src: "/assets/partners/jio-credit.jpg", href: "https://www.jiocredit.in/" }
 ];
 
 export function AssociationLogoMarquee({ compact = false }: { compact?: boolean }) {
